4d7dc81f81a9c9337226aaa621422f82ddc95bd5
Araz Minhas <araz.m6@gmail.com>
2011-11-05 15:13:22 -0700

    Added a title helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -19,6 +19,18 @@ describe PagesController do
       response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => "Sample App | Help")
+    end
+  end
 
   describe "GET 'contact'" do
     it "should be successful" do
